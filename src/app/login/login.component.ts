b2a93ecf36774ede2b75acfffe0dd5228ae3d9a8
SHAHKUL <shahkulhameed@gmail.com>
2025-02-07 23:40:05 +0530

    login form created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  Form,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-login',
-  imports: [NavbarComponent,RouterModule],
+  imports: [NavbarComponent, RouterModule,FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      // name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log('succes', this.userForm.value);
+    } else {
+      console.log('form is invalid');
+    }
+  }
 }
